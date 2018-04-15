329b888b7cd5ec826504606bf9e013cd171c9a4d
bsaig <35349453+bsaig@users.noreply.github.com>
2018-04-15 17:59:54 +0100

    Working with IME Mid Meter and seving in mysql

--- install/files/script/imemodbus.py
+++ install/files/script/imemodbus.py
@@ -6,48 +6,52 @@ import time
 import glob
 
 # Open database connection
-#Sql = ""
-#db = MySQLdb.connect('localhost', 'testuser', 'test123', 'testing');
-#cursor = db.cursor()	# Prepare a cursor object using cursor() method
+Sql = ""
+# Modify the following line with your mysql credentials
+db = MySQLdb.connect('localhost', 'testuser', 'test123', 'test');
+cursor = db.cursor()	# Prepare a cursor object using cursor() method
 
-#Setup Modubus Connection 
+#Setup Modubus Connection
 instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 7) # port name, Slave address
 #instrument.debug = True        #debug mode
 
 def imeCurrent1():
-	current_1 = None
-	while current_1 is None:
+	current1 = None
+	while current1 is None:
 
 		try:
 			while True:
-				current_1 = instrument.read_long(4102)
-				current_1 = round(current_1,2)
-				print "the current on phase 1 is", current_1,"mA"
-				time.sleep(0.11)
+				current1 = instrument.read_long(4102)
+				current1 = round(current1,2)
+				current1 = current1/1000
+				print "The current on phase 1 is", current1,"A"
 		except IOError:
 			pass
 			#print "ioerror"
 		except ValueError:
-			time.sleep(0.11)
-			#print "value error" 
+			#print "value error"
 			pass
+	return current1
 
 def imeVoltage1():
-	voltage = None
-	while voltage is None:
+	voltage1 = None
+	while voltage1 is None:
 
 		try:
 			while True:
-				voltage = instrument.read_long(4096)
-				print "The voltage on phase 1 is", voltage,"mV"
-				time.sleep(0.11)
+				voltage1 = instrument.read_long(4096)
+				voltage1 = round(voltage1,2)
+				voltage1 = voltage1/1000
+				print "The voltage on phase 1 is", voltage1,"V"
+				#time.sleep(0.11)
 		except IOError:
 			#print "IOError"
 			pass
 		except ValueError:
-			#print "Value error" 
-			time.sleep(0.11)
+			#print "Value error"
+			#time.sleep(0.11)
 			pass
+	return voltage1
 
 def imeActivePower():
 	activepower = None
@@ -62,10 +66,11 @@ def imeActivePower():
 			pass
 			#print "IOError"
 		except ValueError:
-			#print "Value error" 
-			time.sleep(0.11)
+			#print "Value error"
+			#time.sleep(0.11)
 			pass
-			
+	return activepower
+
 def imeOperatingTime():
 	operatingTime = None
 	while operatingTime is None:
@@ -74,14 +79,15 @@ def imeOperatingTime():
 			while True:
 				operatingTime = instrument.read_long(4130)
 				print "The time is", operatingTime,"s"
-				time.sleep(0.11)
+				#time.sleep(0.11)
 		except IOError:
 			#print "IOError"
 			pass
 		except ValueError:
-			#print "Value error" 
+			#print "Value error"
 			time.sleep(0.11)
 			pass
+	return operatingTime
 
 def imePhase1Power():
 	phase1power = None
@@ -91,57 +97,52 @@ def imePhase1Power():
 			while True:
 				phase1power = instrument.read_long(4140)
 				print "The power on phase 1 is", phase1power,"mkW"
-				time.sleep(0.11)
+				#time.sleep(0.11)
 		except IOError:
 			#print "IOError"
 			pass
 		except ValueError:
-			#print "Value error" 
-			time.sleep(0.11)
+			#print "Value error"
+			#time.sleep(0.11)
 			pass
-			
+
 def imeFrequency():
 	frequency = None
 	while frequency is None:
 
 		try:
 			while True:
-				frequency = instrument.read_long(1638)
-				print "The power on phase 1 is", phase1power,"mkW"
-				time.sleep(0.11)
+				frequency = instrument.read_register(4134)
+				frequency = round(frequency, 1)
+				frequency = frequency/10
+				print "The frequency is", frequency,"Hz"
 		except IOError:
 			#print "IOError"
 			pass
 		except ValueError:
-			#print "Value error" 
-			time.sleep(0.11)
+			#print "Value error"
 			pass
-			
-			#0x1026 WORD Frequency  #1638 (DEC)
-			
+
+	return frequency
 
 while True:
 
 	try:
-
-		imeActivePower()
-		imeCurrent1()
+		activepower = imeActivePower()
+		current1 = imeCurrent1()
 		imeOperatingTime()
 		imePhase1Power()
-		imeVoltage1()
-		Sql = "insert into current_mesurement1 (time, pcbCurrent, imeCurrent, temperature) values('%s','%f','%f','%f')" % (Time, current_ime, current_pcb, thermistor)
+		voltage1 = imeVoltage1()
+		frequency = imeFrequency()
+		#Create Table "meter" with the bellow columns
+		Sql = "INSERT INTO meter (Current1, Voltage1, Frequency, Energy) VALUES ('%f','%f','%f','%f')" % (current1, voltage1, frequency, activepower)
 		with db:
 
 			cursor.execute(Sql)	# Execute the SQL command
-			
 			print "Database Uploaded"
-
-			db.close()
-			
-			time.sleep(10)
-			
+		db.close()
 		break
-		
+
 
 	except KeyboardInterrupt:
 
